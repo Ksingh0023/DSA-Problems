8ec66f91936f57301a302662d5e3b858a2c7b664
Komal Singh <149627266+Ksingh0023@users.noreply.github.com>
2026-03-22 14:09:52 +0530

    Create 128. Longest Consecutive Sequence .java

---
+++ 128. Longest Consecutive Sequence .java
@@ -0,0 +1,21 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int n:nums) set.add(n);
+
+        int max =0;
+
+        for (int n : set) {
+            if(!set.contains(n - 1)){
+                int curr = n, len = 1;
+
+                while (set.contains(curr + 1)) {
+                    curr++;
+                    len++;
+                }
+                max = Math.max(max, len);
+            }
+        }
+        return max;
+    }
+}
