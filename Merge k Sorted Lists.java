19092f1ba8f5329bc1770b964dbae9bd108b1e23
Komal Singh <149627266+Ksingh0023@users.noreply.github.com>
2026-03-26 20:18:20 +0530

    Create Merge k Sorted Lists.java
    This is a Hard Question

---
+++ Merge k Sorted Lists.java
@@ -0,0 +1,19 @@
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b) -> a.val - b.val);
+
+        for (ListNode l : lists) 
+            if (l != null) pq.add(l);
+
+        ListNode dummy = new ListNode(0), curr = dummy;
+
+        while (!pq.isEmpty()) {
+            ListNode node = pq.poll();
+            curr.next = node;
+            curr = curr.next;
+            if (node.next != null) pq.add(node.next);
+        }
+
+        return dummy.next;
+    }
+}
